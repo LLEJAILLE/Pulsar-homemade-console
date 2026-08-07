fe3a9d804d0bb4e4a7235b10216504245fc0c33a
Louis Lejaille <lejaillelouis@gmail.com>
2026-08-07 17:00:52 +0200

    [add]: release build

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$script_dir"
 
 if [[ ! -f "build/CMakeCache.txt" ]]; then
-    cmake -S . -B build
+    cmake -B build -DCMAKE_BUILD_TYPE=Release
 fi
 
-cmake --build build
+cmake --build build -j4
 
 ./build/Pulsar
