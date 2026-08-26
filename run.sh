84ecc1e28deb1820d80ba8b2f6c3b107f6227053
LLEJAILLE <lejaillelouis@gmail.com>
2026-08-26 16:55:19 +0200

    [edit]: run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$script_dir"
 
-if [[ ! -f "build/CMakeCache.txt" ]]; then
-    cmake -B build -DCMAKE_BUILD_TYPE=Release
-fi
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build -j4
 
