1e95875cb189710b1dfd94a133768d49941e4ecc
LLEJAILLE <lejaillelouis@gmail.com>
2026-08-26 16:44:15 +0200

    [add]: edit

--- src/emulator/LibretroBackend/LibretroEnvironment.cpp
+++ src/emulator/LibretroBackend/LibretroEnvironment.cpp
@@ -5,6 +5,23 @@
 #include <QDir>
 #include "Libretro.h"
 
+#include <cstdarg>
+#include <cstdio>
+
+namespace
+{
+void logCallback(retro_log_level, const char* format, ...)
+{
+    if (!format)
+        return;
+
+    va_list arguments;
+    va_start(arguments, format);
+    std::vfprintf(stderr, format, arguments);
+    va_end(arguments);
+}
+}
+
 retro_pixel_format LibretroEnvironment::m_pixelFormat = RETRO_PIXEL_FORMAT_XRGB8888;
 QHash<QString, QString> LibretroEnvironment::m_requestedOptions;
 QHash<QString, QString> LibretroEnvironment::m_availableOptions;
@@ -28,6 +45,19 @@ retro_pixel_format LibretroEnvironment::pixelFormat()
 bool LibretroEnvironment::callback(unsigned cmd, void* data)
 {
     switch (cmd) {
+    case RETRO_ENVIRONMENT_GET_LOG_INTERFACE:
+    {
+        auto* callback = static_cast<retro_log_callback*>(data);
+        if (!callback)
+            return false;
+
+        callback->log = logCallback;
+        return true;
+    }
+
+    case RETRO_ENVIRONMENT_SET_MESSAGE:
+        return data != nullptr;
+
     case RETRO_ENVIRONMENT_SET_VARIABLES:
     {
         m_availableOptions.clear();
@@ -68,6 +98,63 @@ bool LibretroEnvironment::callback(unsigned cmd, void* data)
         static QHash<QString, QByteArray> optionValues;
         optionValues.insert(key, value.toUtf8());
         variable->value = optionValues[key].constData();
+        return true;
+    }
+
+    case RETRO_ENVIRONMENT_GET_SAVE_DIRECTORY:
+    {
+        static QByteArray saveDirectory;
+        saveDirectory = QDir::toNativeSeparators(Paths::saves()).toLocal8Bit();
+        if (!data)
+            return false;
+
+        *static_cast<const char**>(data) = saveDirectory.constData();
+        return true;
+    }
+
+    case RETRO_ENVIRONMENT_GET_CORE_OPTIONS_VERSION:
+    {
+        if (!data)
+            return false;
+
+        *static_cast<unsigned*>(data) = 1;
+        return true;
+    }
+
+    case RETRO_ENVIRONMENT_SET_CORE_OPTIONS:
+    {
+        const auto* options = static_cast<const retro_core_option_definition*>(data);
+        m_availableOptions.clear();
+        if (!options)
+            return true;
+
+        for (const auto* option = options; option->key; ++option) {
+            QString definition = QString::fromUtf8(option->desc ? option->desc : option->key);
+            definition += QStringLiteral("; ");
+
+            bool firstValue = true;
+            for (const auto& value : option->values) {
+                if (!value.value)
+                    break;
+
+                if (!firstValue)
+                    definition += QStringLiteral("|");
+                definition += QString::fromUtf8(value.value);
+                firstValue = false;
+            }
+
+            m_availableOptions.insert(QString::fromUtf8(option->key), definition);
+        }
+
+        return true;
+    }
+
+    case RETRO_ENVIRONMENT_GET_MESSAGE_INTERFACE_VERSION:
+    {
+        if (!data)
+            return false;
+
+        *static_cast<unsigned*>(data) = 0;
         return true;
     }
 
