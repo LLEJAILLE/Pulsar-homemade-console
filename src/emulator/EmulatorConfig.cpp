d282f77ee9c6ccab497eccbfa8603b132dfec239
LLEJAILLE <lejaillelouis@gmail.com>
2026-08-31 22:19:10 +0200

    [add]: block size

--- src/emulator/EmulatorConfig.cpp
+++ src/emulator/EmulatorConfig.cpp
@@ -31,6 +31,8 @@ QMap<QString, QString> EmulatorConfig::getHighPerformanceConfig()
     QMap<QString, QString> config;
 
     config["melonds_jit_enable"] = "enabled";
+    config["melonds_jit_block_size"] = "32";
+    
     config["melonds_render_mode"] = "software";
     config["melonds_threaded_renderer"] = "enabled";
     config["melonds_audio_interpolation"] = "disabled";
